81e6efddaf3bb533decf5ab35b20c573738da01c
CodingBlyat69 <98026411+NetherWartQOL@users.noreply.github.com>
2022-05-16 16:22:27 +0200

    Update xray.js

--- xray.js
+++ xray.js
@@ -1,3 +1,10 @@
-const canvas = document.getElementById('game_body');
+window.setInterval(function() {
+const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
 ctx.globalAlpha = 0.5;
+
+    const canvas2 = document.getElementById('game_body');
+const ctx2 = canvas2.getContext('2d');
+ctx2.globalAlpha = 0.5;
+  // do stuff
+}, 100); // 1000 milliseconds (1 second)
